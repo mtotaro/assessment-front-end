f5b8db2bdd857dc1bfb235807252bb49cb0059e8
Matias Totaro <matiastotaro@gmail.com>
2019-08-02 20:18:45 -0300

    wrap app with Provider, created some constants

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader'; // i install this so i can see the change of my running app while keeping the components mounted
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Wrap the rendering in a function:
+const render = (Component) => {
+  ReactDOM.render(
+    // Wrap App inside AppContainer
+    <AppContainer>
+      <Component />
+    </AppContainer>,
+    document.getElementById('root'),
+  );
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+// Do this once
 serviceWorker.unregister();
+
+// Render once
+render(App);
+
+// Webpack Hot Module Replacement API
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    render(App);
+  });
+}
